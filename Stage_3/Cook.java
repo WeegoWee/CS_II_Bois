a0ac2ca5bf99630663e3788071e91d7900e6542f
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-10-20 14:28:26 -0600

    Update Cook.java

--- Stage_3/Cook.java
+++ Stage_3/Cook.java
@@ -1,12 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author estra
- */
-public class Cook {
+public class Cook extends Individual{
+    private String order;
+    private String name;
+    private String employeeID;
+    private Individual contactinfo;
+    
+    public Cook(String name, String contactInfo) {
+        super(name, contactInfo);
+    }
+
+    public Cook(String order, String name, String employeeID, String contactInfo) {
+        super(name, contactInfo);
+        this.order = order;
+        this.name = name;
+        this.employeeID = employeeID;
+        
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Individual getContactinfo() {
+        return contactinfo;
+    }
+
+    public void setContactinfo(Individual contactinfo) {
+        this.contactinfo = contactinfo;
+    }
+    
+    public void showInfo(){
+        
+    }
     
 }
