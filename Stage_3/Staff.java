6af00cbb7fc01aeeb003a07225e8c9da4d94d3b2
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-10-20 14:27:16 -0600

    Small Changes

--- Stage_3/Staff.java
+++ Stage_3/Staff.java
@@ -1,12 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author estra
- */
-public class Staff {
+public class Staff extends Individual{
+    private String employeeID;
+    
+    public Staff(String name, String contactInformation) {
+        super(name, contactInformation);
+    }
+    
+    public void duties(){
+        
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
     
 }
