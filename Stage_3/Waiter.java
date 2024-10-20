1d94924fea505adedf2431f28339895817bcdd5a
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-10-20 14:29:15 -0600

    Update Waiter.java

--- Stage_3/Waiter.java
+++ Stage_3/Waiter.java
@@ -1,12 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author estra
- */
 public class Waiter {
+    private String order;
+    private String name;
+    private String employeeID;
+    private Individual contactinfo;
+
+    public Waiter(String order, String name, String employeeID, Individual contactinfo) {
+        this.order = order;
+        this.name = name;
+        this.employeeID = employeeID;
+        this.contactinfo = contactinfo;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Individual getContactinfo() {
+        return contactinfo;
+    }
+
+    public void setContactinfo(Individual contactinfo) {
+        this.contactinfo = contactinfo;
+    }
+    
+    public void showInfo(){
+        
+    }
+    
     
 }
