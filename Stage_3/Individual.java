a315eb6520a49a7f913a1ce1b48b6f959aab4282
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-10-20 12:53:56 -0600

    Pushing two classes

---
+++ Stage_3/Individual.java
@@ -0,0 +1,30 @@
+
+public class Individual {
+    
+    private String name;
+    private String contactInformation;
+    
+    public Individual(String name, String contactInformation){
+        this.name = name;
+        this.contactInformation = contactInformation;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+    
+    
+}
