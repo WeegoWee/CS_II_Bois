f717e35ab8d0d662cb06cb17aa2a3d0518539442
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-11-09 21:37:24 -0700

    Update Individual.java

--- Stage_3/Individual.java
+++ Stage_3/Individual.java
@@ -1,24 +1,30 @@
+//this is the Individual Class
 public class Individual {
     private String name;
     private String role;
 
+    //the individual constructor initializes the instance variables
     public Individual(String name, String role) {
         this.name = name;
         this.role = role;
     }
 
+    //this method gets the name and returns the name
     public String getName() {
         return name;
     }
 
+    //this method gets the role and returns the role
     public String getRole() {
         return role;
     }
 
+    //this method sets the name.
     public void setName(String name) {
         this.name = name;
     }
 
+    //this method sets the role.
     public void setRole(String role) {
         this.role = role;
     }
