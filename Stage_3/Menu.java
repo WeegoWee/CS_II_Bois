8fc56790e37e0f9b98116f05fb9855c401aa2061
WeegoWee <rigoberto.estradaolivas@enmu.edu>
2024-11-09 22:09:26 -0700

    Update Menu.java

--- Stage_3/Menu.java
+++ Stage_3/Menu.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
-
+//this is the menu class
 public class Menu {
     
     private ArrayList<Inventory> items;
     
 
+    //the constructor initializes the array list
     public Menu() {
         this.items = new ArrayList<>();
         
     }
 
+    //this methd adds new items to the list
     public void addItems(Inventory item) {
         items.add(item);
     }
 
+    //this method removes items from the list
     public void removeItems(Inventory item) {
         items.remove(item);
     }
 
+    //this method gets the items from the array list
     public ArrayList<Inventory> getItems() {
         return items;
     }
 
+    //this method prints the menu
     public void showMenu() {
         for (Inventory item : items) {
             System.out.println(item.getItems() + " - $" + item.getPrice() + " (Available | " + item.getTotalItems() + ")");
